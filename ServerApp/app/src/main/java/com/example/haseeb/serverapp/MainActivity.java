53b366c0da6684e6eb2f7b549bf302c7bfd9d01f
Haseeb Ehsan <haseeb_ehsan@hotmail.com>
2017-04-21 15:41:54 +0500

    everything commited

--- ServerApp/app/src/main/java/com/example/haseeb/serverapp/MainActivity.java
+++ ServerApp/app/src/main/java/com/example/haseeb/serverapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.haseeb.serverapp;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -22,17 +24,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
+       // ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE},1);
 
         try {
              myServer = new ServerSocket(9898);
-            while(true) {
-                conn = myServer.accept();
-                in = conn.getInputStream();
-                input = new BufferedReader(new InputStreamReader(in));
-
-                tv1 = (TextView)findViewById(R.id.tv1);
-                tv1.setText(input.readLine());
-            }
+            conn = myServer.accept();
+//            while(true) {
+////                conn = myServer.accept();
+////                in = conn.getInputStream();
+////                input = new BufferedReader(new InputStreamReader(in));
+////
+////                tv1 = (TextView)findViewById(R.id.tv1);
+////                tv1.setText(input.readLine());
+//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
