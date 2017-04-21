66081ac3285690263206559f72d7ebb13f84ba3f
Haseeb Ehsan <haseeb_ehsan@hotmail.com>
2017-04-21 15:10:47 +0500

    added Connection Code uncompleted

--- ServerApp/app/src/main/java/com/example/haseeb/serverapp/MainActivity.java
+++ ServerApp/app/src/main/java/com/example/haseeb/serverapp/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.haseeb.serverapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
     ServerSocket myServer;
+    InputStream in;
+    BufferedReader input;
+    Socket conn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
         try {
              myServer = new ServerSocket(9898);
+            conn = myServer.accept();
+            in = conn.getInputStream();
+            in.read();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+
     }
 }
