8ec8adf9f6a4429b951244c704e32dc8a0155078
Haseeb Ehsan <haseeb_ehsan@hotmail.com>
2017-04-21 19:08:20 +0500

    trying with sockets

--- ServerApp/app/src/main/java/com/example/haseeb/serverapp/MainActivity.java
+++ ServerApp/app/src/main/java/com/example/haseeb/serverapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.haseeb.serverapp;
 
 import android.Manifest;
+import android.os.StrictMode;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -24,22 +26,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
        // ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE},1);
 
         try {
              myServer = new ServerSocket(9898);
-            conn = myServer.accept();
-//            while(true) {
-////                conn = myServer.accept();
-////                in = conn.getInputStream();
-////                input = new BufferedReader(new InputStreamReader(in));
-////
-////                tv1 = (TextView)findViewById(R.id.tv1);
-////                tv1.setText(input.readLine());
-//            }
+            Log.e("Exception", "In the try");
+           // conn = myServer.accept();
+            while(true) {
+                Log.e("Exception", "In the loop");
+                conn = myServer.accept();
+                in = conn.getInputStream();
+                input = new BufferedReader(new InputStreamReader(in));
+
+                tv1 = (TextView)findViewById(R.id.tv1);
+                tv1.setText(input.readLine());
+                Log.e("Exception", "after setuopo");
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            Log.e("Exception", "General Exception"+e.toString());
         }
 
 
