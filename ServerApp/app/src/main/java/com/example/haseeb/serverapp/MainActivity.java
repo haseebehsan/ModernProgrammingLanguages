3a89742a6b3f1b24faea02c7017244ec64627866
Haseeb Ehsan <haseeb_ehsan@hotmail.com>
2017-04-21 15:04:08 +0500

    permissions Added

--- ServerApp/app/src/main/java/com/example/haseeb/serverapp/MainActivity.java
+++ ServerApp/app/src/main/java/com/example/haseeb/serverapp/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.haseeb.serverapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class MainActivity extends AppCompatActivity {
+    ServerSocket myServer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+             myServer = new ServerSocket(9898);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
