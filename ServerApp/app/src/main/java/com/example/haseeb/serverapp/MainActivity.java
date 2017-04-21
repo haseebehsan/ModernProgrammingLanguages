466fb69f2ff6c7dd6b4f1372a9eb502a0d08a056
Haseeb Ehsan <haseeb_ehsan@hotmail.com>
2017-04-21 15:14:36 +0500

    Added code of reading input in a while loop

--- ServerApp/app/src/main/java/com/example/haseeb/serverapp/MainActivity.java
+++ ServerApp/app/src/main/java/com/example/haseeb/serverapp/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.haseeb.serverapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     ServerSocket myServer;
     InputStream in;
     BufferedReader input;
+    TextView tv1;
     Socket conn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +25,14 @@ public class MainActivity extends AppCompatActivity {
 
         try {
              myServer = new ServerSocket(9898);
-            conn = myServer.accept();
-            in = conn.getInputStream();
-            in.read();
+            while(true) {
+                conn = myServer.accept();
+                in = conn.getInputStream();
+                input = new BufferedReader(new InputStreamReader(in));
+
+                tv1 = (TextView)findViewById(R.id.tv1);
+                tv1.setText(input.readLine());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
