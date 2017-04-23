7c9a5f24a4a8d44d4a4915bca7efe3488b3b89b3
Haseeb Ehsan <haseeb_ehsan@hotmail.com>
2017-04-23 17:10:08 +0500

    Database in working condition

--- LocationPathDB/app/src/main/java/com/example/haseeb/locationpathdb/MainActivity.java
+++ LocationPathDB/app/src/main/java/com/example/haseeb/locationpathdb/MainActivity.java
@@ -5,8 +5,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -14,34 +16,68 @@ import static android.content.Context.MODE_PRIVATE;
 public class MainActivity extends AppCompatActivity {
     Intent intent;
     TextView tv1;
+    SQLiteDatabase db;
+    String no = "s_no";
+    String name_id="name";
+    int val1;
+    String val2;
+    EditText num, val;
+    String s1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        SQLiteDatabase db = openOrCreateDatabase("Teacher", MODE_PRIVATE, null);
-        db.execSQL("create table StudentTab(s_no integer, name varchar(20))");
-        String no="s_no";
-        int val1=10;
-        String name_id="name";
-        String val2="Mubeen";
-        db.execSQL("insert into StudentTab(" + no + ","+ name_id + ") values (" +val1 +","+ "'"+val2+"'" + ");");
+        db = openOrCreateDatabase("Teacher", MODE_PRIVATE, null);
+        //db.execSQL("create table StudentTab(s_no integer, name varchar(20))");
+        num = (EditText)findViewById(R.id.number);
+        val = (EditText)findViewById(R.id.name);
+        val1=11;
+
+        val2="Haseeb";
+
         Cursor cursor = db.rawQuery("select * from StudentTab", null);
-        String s1="";
+        s1="";
         if (cursor.moveToFirst()) {
+
+            Log.e("msg",""+cursor.getPosition() );
             do {
-                s1 = s1 +" "+ cursor.getString(cursor.getColumnIndex(no))+" "
+                s1 = s1 +"\n"+ cursor.getString(cursor.getColumnIndex(no))+" "
                         + cursor.getString(cursor.getColumnIndex(name_id));
             } while (cursor.moveToNext());
         }
         tv1 = (TextView)findViewById(R.id.tv1);
         tv1.setText(s1);
-        
+
+
 
 
 
         intent = new Intent(this,backgroundTask.class);
+
+        Button addButton = (Button)findViewById(R.id.add);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                val2 = val.getText().toString();
+                val1 = Integer.parseInt(num.getText().toString());
+
+                db.execSQL("insert into StudentTab(" + no + ","+ name_id + ") values (" +val1 +","+ "'"+val2+"'" + ");");
+
+                Cursor cursor = db.rawQuery("select * from StudentTab", null);
+                String s1="";
+                if (cursor.moveToFirst()) {
+                    do {
+                        s1 = s1 +"\n"+ cursor.getString(cursor.getColumnIndex(no))+" "
+                                + cursor.getString(cursor.getColumnIndex(name_id));
+                    } while (cursor.moveToNext());
+                }
+                tv1 = (TextView)findViewById(R.id.tv1);
+                tv1.setText(s1);
+
+            }
+        });
 
         Button stopbutton = (Button) findViewById(R.id.stopbutton);
         // stopbutton.setText("Stop");
