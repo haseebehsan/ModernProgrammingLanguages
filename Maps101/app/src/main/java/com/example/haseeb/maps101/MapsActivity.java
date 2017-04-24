c898218457f5b2409bc844462dddc781f36c247f
Haseeb Ehsan <haseeb_ehsan@hotmail.com>
2017-04-24 17:36:32 +0500

    key added

--- Maps101/app/src/main/java/com/example/haseeb/maps101/MapsActivity.java
+++ Maps101/app/src/main/java/com/example/haseeb/maps101/MapsActivity.java
@@ -39,8 +39,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(33.65703349, 73.15764405);
+
+        
+        mMap.addMarker(new MarkerOptions().position(sydney).title("hostel"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
